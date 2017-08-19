e531ae294c5e69d810d1152cc262fa8c5a63652e
aric <aric.allen2@gmail.com>
2017-08-19 13:12:27 -0700

    workspace bullshit

--- src/musetest/musetest.java
+++ src/musetest/musetest.java
@@ -4,8 +4,14 @@ import processing.core.PApplet;
 import oscP5.*;
 
 public class musetest extends PApplet {
+
+    int recvPort = 5000;
+    OscP5 oscP5;
+
     public void settings() {
         size(200, 200);
+//        frameRate(60);
+        oscP5 = new OscP5(this, recvPort);
     }
 
     public void draw() {
@@ -13,8 +19,13 @@ public class musetest extends PApplet {
         ellipse(mouseX, mouseY, 20, 20);
     }
 
-    public void oscEvent(event) {
-
+    void oscEvent(OscMessage msg) {
+        System.out.println("### got a message " + msg);
+        if (msg.checkAddrPattern("/muse/eeg")) {
+            for (int i = 0; i < 4; i++) {
+                System.out.print("EEG on channel " + i + ": " + msg.get(i).floatValue() + "\n");
+            }
+        }
     }
 
     public static void main(String... args) {
