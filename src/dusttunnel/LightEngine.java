f3d5d7f9b37c10bc32a05673b6d1326b09fdf252
aricallen <aric.allen2@gmail.com>
2017-08-21 22:15:40 -0700

    light engine sent off status when headset is off

--- src/dusttunnel/LightEngine.java
+++ src/dusttunnel/LightEngine.java
@@ -17,6 +17,8 @@ public class LightEngine {
     private int _status = 0;
 
     private String _addressPattern;
+    private float _last;
+
     final String HOST = "10.0.1.5";
     final int PORT = 9000;
 
@@ -24,41 +26,46 @@ public class LightEngine {
     final float FADE_OUT_AMOUNT = 0.05f;
 
     public LightEngine(OscP5 oscP5) {
-//        super(models);
-//        _models = models;
         _oscP5 = oscP5;
         remoteAddress = new NetAddress(HOST, PORT);
     }
 
+    private void setLast(float value) {
+        _last = value;
+    }
+
     public void update(MuseModel model) {
         if (model.getAddressPattern() == "/muse/elements/alpha_absolute") {
-            send(model.getUserValue(), "/sean/alpha");
+            float value = model.getUserValue();
+            send(value, "/sean/alpha");
+            setLast(value);
         }
 
         if (model.getAddressPattern() == "/muse/elements/gamma_absolute") {
-            send(model.getUserValue(), "/sean/beta");
+            float value = model.getUserValue();
+            send(value, "/sean/beta");
+            setLast(value);
         }
     }
 
     public void reset() {
-        setStatus(0);
-
-        TimerTask resetTask = new TimerTask() {
-            public void run() {
-                fadeOut();
-            }
-        };
-
-        timer.scheduleAtFixedRate(resetTask, 0L, FADE_OUT_RATE);
+//        TimerTask resetTask = new TimerTask() {
+//            public void run() {
+//                fadeOut();
+//            }
+//        };
+//
+//        timer.scheduleAtFixedRate(resetTask, 0L, FADE_OUT_RATE);
+        send(0.0f, "/muse/elements/touching_forehead");
     }
 
     private void fadeOut() {
-//        float value = getLast() - FADE_OUT_AMOUNT;
-//        setLast(value);
-//        if (value <= 0) {
-//            timer.cancel();
-//            send(0, 0.0f);
-//        }
+        float value = _last - FADE_OUT_AMOUNT;
+        setLast(value);
+        if (value <= 0) {
+            timer.cancel();
+            send(0.0f, "/muse/elements/touching_forehead");
+        }
     }
 
     /**
@@ -68,24 +75,8 @@ public class LightEngine {
         _status = status;
     }
 
-    private int getStatus() {
-        return _status;
-    }
-
-    public void setValueK(float k) {
-        _valueK = k;
-    }
-
-    private String getAddressPattern() {
-        return _addressPattern;
-    }
-
-    public void setAddressPattern(String addressPattern) {
-        _addressPattern = addressPattern;
-    }
-
     private void send(float value, String addressPattern) {
-        OscMessage msg = new OscMessage("/sean/alpha");
+        OscMessage msg = new OscMessage(addressPattern);
         msg.add(value);
         _oscP5.send(msg, remoteAddress);
     }
